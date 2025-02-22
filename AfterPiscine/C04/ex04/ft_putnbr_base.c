ce380db32f4912ace997f0d93d50da78eac1b422
chaudbrush <chaudbrush@gmail.com>
2025-02-22 18:30:24 +0000

    C04

--- AfterPiscine/C04/ex04/ft_putnbr_base.c
+++ AfterPiscine/C04/ex04/ft_putnbr_base.c
@@ -1,49 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vloureir <vloureir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/06 07:58:09 by vloureir          #+#    #+#             */
-/*   Updated: 2025/02/06 09:57:13 by vloureir         ###   ########.fr       */
+/*   Created: 2025/02/22 15:30:02 by vloureir          #+#    #+#             */
+/*   Updated: 2025/02/22 16:44:05 by vloureir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include <unistd.h>
-
-void	ft_putnbr_rec(long nb_l, int len, char *base)
-{
-	int	digit;
-
-	if (nb_l == 0)
-		return ;
-	digit = (nb_l % len);
-	ft_putnbr_rec(nb_l / len, len, base);
-	write(1, &base[digit], 1);
-}
 
 int	ft_strlen(char *str)
 {
 	int	i;
 
 	i = 0;
-	while (str[i])
+	while(str[i])
 		i++;
 	return (i);
 }
 
-int	check_base(char *base, int len)
+int	ft_check_base(char *base, int len)
 {
 	int	i;
 	int	j;
 
 	i = 0;
-	while(base[i] && i < len)
+	while (base[i] && i < len)
 	{
-		j = i + 1;
 		if (base[i] == '-' || base[i] == '+')
 			return (0);
+		j = i + 1;
 		while (base[j])
 		{
 			if (base[i] == base[j])
@@ -55,28 +45,45 @@ int	check_base(char *base, int len)
 	return (1);
 }
 
+void	ft_rec_putnbr(long nb_l, char *base, int len)
+{
+	char	digit;
+
+	if (nb_l == 0)
+		return ;
+	digit = (nb_l % len);
+	ft_rec_putnbr((nb_l / len), base, len);
+	write(1, &base[digit], 1);
+}
+
 void	ft_putnbr_base(int nbr, char *base)
 {
-	int		len;
 	long	nb_l;
+	int		len;
 
 	len = ft_strlen(base);
-	if (len < 2)
-		return ;
-	if (check_base(base, len) == 0)
-		return ;
 	nb_l = nbr;
+	if (len < 2 || ft_check_base(base, len) == 0)
+		return ;
 	if (nb_l == 0)
-		write(1, &base[0], 1);
+	{
+		write(1, "0", 1);
+		return ;
+	}
 	if (nb_l < 0)
 	{
 		write(1, "-", 1);
 		nb_l *= -1;
 	}
-	ft_putnbr_rec(nb_l, len, base);
+	ft_rec_putnbr(nb_l, base, len);
 }
-/*
+
 int	main(void)
 {
-	ft_putnbr_base(-33254, "0123456789abcdef");
-}*/
+	ft_putnbr_base(127, "0123456789abcdef");
+	write(1, "\n", 1);
+	ft_putnbr_base(127, "01");
+	write(1, "\n", 1);
+	ft_putnbr_base(127, "01234567");
+	write(1, "\n", 1);
+}
---
+++ AfterPiscine/C04/ex05/ft_atoi_base.c
@@ -0,0 +1,106 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi_base.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vloureir <vloureir@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/22 16:48:20 by vloureir          #+#    #+#             */
+/*   Updated: 2025/02/22 18:20:41 by vloureir         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+int	get_int_value(char *base, char c)
+{
+	if ((c - '0') > 9)
+		c = (c - 'a') + 10;
+	else
+		c -= '0';
+	return (c);
+}
+
+int	char_in_base(char *base, char c)
+{
+	int	i;
+
+	i = 0;
+	while (base[i])
+	{
+		if (c == base[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	ft_check_base(char *str)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '-' || str[i] == '+' || str[i] == ' '
+			|| (str[i] >= 9 && str[i] <= 13))
+			return (0);
+		j = i + 1;
+		while (str[j])
+		{
+			if (str[i] == str[j])
+				return (0);
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	ft_atoi_base(char *str, char *base)
+{
+	int	i;
+	int	nb;
+	int	sign;
+	int	len;
+
+	i = 0;
+	nb = 0;
+	sign = 1;
+	len = ft_strlen(base);
+	if (len < 2 || ft_check_base(base) == 0)
+		return (0);
+	while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
+		i++;
+	while (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			sign = -sign;
+		i++;
+	}
+	while (char_in_base(base, str[i]) && str[i])
+	{
+		nb = (nb * len) + (get_int_value(base, str[i]));
+		i++;
+	}
+	return (nb * sign);
+}
+/*
+int	main(void)
+{
+	printf("%d\n", ft_atoi_base("   ---ff", "0123456789abcdef"));
+	printf("%d\n", ft_atoi_base("   --1111111", "01"));
+	printf("%d\n", ft_atoi_base("   --177", "01234567"));
+	printf("%d\n", ft_atoi_base("   --0", "01234567"));
+}*/
