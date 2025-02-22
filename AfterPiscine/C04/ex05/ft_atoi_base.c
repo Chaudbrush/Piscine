f0687f0eca2b04b7f872f93fa162eedf4ed73493
chaudbrush <chaudbrush@gmail.com>
2025-02-22 19:54:25 +0000

    atoi base more examples

--- AfterPiscine/C04/ex05/ft_atoi_base.c
+++ AfterPiscine/C04/ex05/ft_atoi_base.c
@@ -6,7 +6,7 @@
 /*   By: vloureir <vloureir@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/22 16:48:20 by vloureir          #+#    #+#             */
-/*   Updated: 2025/02/22 18:20:41 by vloureir         ###   ########.fr       */
+/*   Updated: 2025/02/22 19:53:45 by vloureir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,16 @@ int	ft_strlen(char *str)
 
 int	get_int_value(char *base, char c)
 {
-	if ((c - '0') > 9)
-		c = (c - 'a') + 10;
-	else
-		c -= '0';
-	return (c);
+	int	i;
+
+	i = 0;
+	while (base[i])
+	{
+		if (c == base[i])
+			return (i);
+		i++;
+	}
+	return (0);
 }
 
 int	char_in_base(char *base, char c)
@@ -100,7 +105,17 @@ int	ft_atoi_base(char *str, char *base)
 int	main(void)
 {
 	printf("%d\n", ft_atoi_base("   ---ff", "0123456789abcdef"));
-	printf("%d\n", ft_atoi_base("   --1111111", "01"));
+	printf("%d\n", ft_atoi_base("   ---2a", "0123456789abcdef"));
+	printf("%d\n", ft_atoi_base("   ---cafe", "0123456789abcdef"));
+	printf("%d\n", ft_atoi_base("   7fffffff", "0123456789abcdef"));
+	printf("%d\n", ft_atoi_base("   80000000", "0123456789abcdef"));
+	printf("%d\n", ft_atoi_base("   --01111111111111111111111111111111", "01"));
+	printf("%d\n", ft_atoi_base("   ---10000000000000000000000000000000", "01"));
+	printf("%d\n", ft_atoi_base("   ----101010", "01"));
+	printf("%d\n", ft_atoi_base("   ----1", "01"));
+	printf("%d\n", ft_atoi_base("   ----01", "01"));
 	printf("%d\n", ft_atoi_base("   --177", "01234567"));
+	printf("%d\n", ft_atoi_base("   --177", "01234567-"));
 	printf("%d\n", ft_atoi_base("   --0", "01234567"));
+	printf("%d\n", ft_atoi_base("   ;.;.;.", ".;"));
 }*/
